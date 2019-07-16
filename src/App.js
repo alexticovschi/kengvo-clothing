2b31259996d65c9c8f4944504770aa8fdd577e8c
AlexTicovschi <alexmticovschi@gmail.com>
2019-07-16 09:53:38 +0100

    creating the ShopPage component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './Pages/HomePage';
+import ShopPage from './Pages/ShopPage';
 
 function App() {
   return ( 
     <Switch>
       <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
     </Switch>
   );
 }
