4d004bc130524c00038aa4728e0d83bdd4cf1ed9
AlexTicovschi <alexmticovschi@gmail.com>
2019-07-22 19:41:16 +0100

    implementing cart reducer

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -7,8 +7,7 @@ import CartIcon from "../CartIcon";
 import CardDropdown from "../CartDropdown";
 import "./styles.scss";
 
-const Header = () => {
-  const { currentUser } = auth;
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -32,13 +31,14 @@ const Header = () => {
         )}
         <CartIcon />
       </div>
-      <CardDropdown/>
+      {hidden ? null : <CardDropdown />}
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/user/user-actions.js
+++ src/redux/user/user-actions.js
@@ -1,4 +1,6 @@
-import { UserActionTypes } from './user-types';
+import {
+    UserActionTypes
+} from './user-types';
 
 
 export const setCurrentUser = user => ({
