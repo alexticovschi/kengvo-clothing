11d73cb05e7bb2afeeaafeb240b9a139eb856c1c
AlexTicovschi <alexmticovschi@gmail.com>
2019-07-23 14:41:40 +0100

    add multiple items to cart & update items quantity

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,4 +1,7 @@
 import CartActionTypes from './cart-types';
+import {
+    addItemToCart
+} from './utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -15,7 +18,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
             default:
                 return state;
