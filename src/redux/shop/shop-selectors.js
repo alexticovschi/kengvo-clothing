5d7f32935b4504da981966ff5a443e177c450c62
AlexTicovschi <alexmticovschi@gmail.com>
2019-08-09 11:58:31 +0100

    updating data flow

--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -8,6 +8,13 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    // get all the object keys and then map over that array of keys 
+    // so we get the value of the collections object at that key
+    collections => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
