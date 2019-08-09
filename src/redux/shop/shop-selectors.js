f83e7a72e174c8312adde45dcc74c8164f6bf32a
AlexTicovschi <alexmticovschi@gmail.com>
2019-08-09 11:26:14 +0100

    data normalization - storing a list of objects

--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -1,28 +1,15 @@
-import {
-    createSelector
-} from "reselect";
+import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    jackets: 2,
-    sneakers: 3,
-    womens: 4,
-    mens: 5
-};
 
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+  [selectShop],
+  shop => shop.collections
 );
 
 export const selectCollection = collectionUrlParam =>
-    createSelector(
-        [selectCollections],
-        collections =>
-        // find collection.id matching the url parameter of the collection id map
-        collections.find(
-            collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-        )
-    );
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
